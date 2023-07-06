2df0c53be0de0ae28b57dd69c6181f4ff00beac2
HSteffensen <henry@steffensenfamily.com>
2023-07-06 08:17:26 +0200

    add login

---
+++ src/routes/login/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
+</script>
+
+<nav class="flex justify-center my-6">
+	<ul class="steps">
+		<a href="/login" class="step step-primary">Sign In</a>
+		<a
+			href="/login/username"
+			class="step"
+			class:step-primary={$page.route.id?.match(/username|photo/g)}
+		>
+			Choose Username
+		</a>
+		<a href="/login/photo" class="step" class:step-primary={$page.route.id?.includes("photo")}>
+			Upload Photo
+		</a>
+	</ul>
+</nav>
+
+<AnimatedRoute>
+	<main class="w-4/6 mx-auto card bg-neutral text-neutral-content">
+		<div class="items-center text-center card-body">
+			<slot />
+		</div>
+	</main>
+</AnimatedRoute>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { signInWithGoogle } from "$lib/auth";
+	import { user, auth } from "$lib/firebase";
+	import { signOut } from "firebase/auth";
+</script>
+
+<h2>Login</h2>
+
+{#if $user}
+	<h2 class="card-title">Welcome, {$user.displayName}</h2>
+	<p class="text-center text-success">You are logged in</p>
+	<button class="btn btn-danger" on:click={() => signOut(auth)}>Sign out</button>
+{:else}
+	<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+{/if}
